62efc7159691ed207f9c82ad78305d9f20a6de72
Usmonbek Ravshanov <Usmonbek_Ravshanov@epam.com>
2021-12-11 12:12:17 +0500

    Single Responsibility principle applied

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+'''
+    1. Applying first principle: Single responsibility principle
+        
+        Class Order contains responsibility for both containning orders and processing payments 
+        which violates the principle above. 
+        Thus we seperate out pay method to seperate class named PaymentProcessor. 
+        Moreorver, the pay methods itself is not responsible for single item, instead it's both handling
+        debit and credit card payments. Likewise, we create seperate methods for each payment type.
+
+'''
+
 class Order:
     items = []
     quantities = []
@@ -11,15 +22,26 @@ class Order:
     
     def total_price(self):
         return sum(map(lambda p, q: p*q, self.prices, self.quantities))
+
+
+class PaymentProcessor:
+    def pay_debit(self, order, security_code):
+        print("Processing debit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
     
-    def pay(self, payment_type, security_code):
-        if payment_type == "debit":
-            print("Processing debit payment type")
-            print(f"Verifying security code: {security_code}")
-            self.status = "paid"
-        elif payment_type == "credit":
-            print("Processing credit payment type")
-            print(f"Verifying security code: {security_code}")
-            self.status="paid"
-        else:
-            raise Exception(f"Unknown payment type: {payment_type}")
+    def pay_credit(self, order, security_code):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status="paid"
+
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+
+processor = PaymentProcessor()
+print(order.total_price())
+processor.pay_debit(order, "0989795")
+
